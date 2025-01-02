6d1c3ac1d112d9a8fa42b3096f80ea0b0b253577
Rifqi Tarmono <rifqitarmono46@gmail.com>
2025-01-03 00:54:26 +0700

    Update vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // vite.config.js
 export default defineConfig({
   base: '/portofolio-2025/',  // Ganti dengan nama repositori Anda
-  build: {
-    outDir: 'dist',
-  },
   plugins: [react()],
 });
