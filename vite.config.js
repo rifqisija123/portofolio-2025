c5a7065dd40de3316cc573752c38c2b9377df5fc
rifqisija123 <rifqitarmono46@gmail.com>
2025-01-02 23:51:21 +0700

    update new vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // vite.config.js
 export default defineConfig({
-  base: '/portofolio-2025/',  // Ganti dengan nama repositori Anda
+  base: '/',  // Ganti dengan nama repositori Anda
   build: {
     outDir: 'dist',
   },
